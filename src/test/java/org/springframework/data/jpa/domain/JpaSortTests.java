5d6369cf7afb938471735e18a8856a24bf067751
cloud <1139509801@qq.com>
2020-07-27 10:22:39 +0800

    hehe

---
+++ src/test/java/org/springframework/data/jpa/domain/JpaSortTests.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.data.domain.Sort.Direction.*;
+import static org.springframework.data.jpa.domain.JpaSort.*;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.metamodel.Attribute;
+import javax.persistence.metamodel.PluralAttribute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.data.jpa.domain.JpaSort.JpaOrder;
+import org.springframework.data.jpa.domain.JpaSort.Path;
+import org.springframework.data.jpa.domain.sample.Address_;
+import org.springframework.data.jpa.domain.sample.MailMessage_;
+import org.springframework.data.jpa.domain.sample.MailSender_;
+import org.springframework.data.jpa.domain.sample.User_;
+import org.springframework.lang.Nullable;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Integration tests for {@link JpaSort}. This has to be an integration test due to the design of the statically
+ * generated meta-model classes. The properties cannot be referred to statically (quite a surprise, as they're static)
+ * but only after they've been enhanced by the persistence provider. This requires an {@link EntityManagerFactory} to be
+ * bootstrapped.
+ *
+ * @author Thomas Darimont
+ * @author Oliver Gierke
+ * @author Christoph Strobl
+ * @author Jens Schauder
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:infrastructure.xml")
+public class JpaSortTests {
+
+	private static final @Nullable Attribute<?, ?> NULL_ATTRIBUTE = null;
+	private static final Attribute<?, ?>[] EMPTY_ATTRIBUTES = new Attribute<?, ?>[0];
+
+	private static final @Nullable PluralAttribute<?, ?, ?> NULL_PLURAL_ATTRIBUTE = null;
+	private static final PluralAttribute<?, ?, ?>[] EMPTY_PLURAL_ATTRIBUTES = new PluralAttribute<?, ?, ?>[0];
+
+	@Test(expected = IllegalArgumentException.class) // DATAJPA-12
+	public void rejectsNullAttribute() {
+		JpaSort.of(NULL_ATTRIBUTE);
+	}
+
+	@Test(expected = IllegalArgumentException.class) // DATAJPA-12
+	public void rejectsEmptyAttributes() {
+		JpaSort.of(EMPTY_ATTRIBUTES);
+	}
+
+	@Test(expected = IllegalArgumentException.class) // DATAJPA-12
+	public void rejectsNullPluralAttribute() {
+		JpaSort.of(NULL_PLURAL_ATTRIBUTE);
+	}
+
+	@Test(expected = IllegalArgumentException.class) // DATAJPA-12
+	public void rejectsEmptyPluralAttributes() {
+		JpaSort.of(EMPTY_PLURAL_ATTRIBUTES);
+	}
+
+	@Test // DATAJPA-12
+	public void sortBySinglePropertyWithDefaultSortDirection() {
+		assertThat(JpaSort.of(path(User_.firstname))).contains(Order.asc("firstname"));
+	}
+
+	@Test // DATAJPA-12
+	public void sortByMultiplePropertiesWithDefaultSortDirection() {
+		assertThat(JpaSort.of(User_.firstname, User_.lastname)).contains(Order.asc("firstname"), Order.asc("lastname"));
+	}
+
+	@Test // DATAJPA-12
+	public void sortByMultiplePropertiesWithDescSortDirection() {
+
+		assertThat(JpaSort.of(DESC, User_.firstname, User_.lastname)).contains(new Order(DESC, "firstname"),
+				Order.desc("lastname"));
+	}
+
+	@Test // DATAJPA-12
+	public void combiningSortByMultipleProperties() {
+
+		assertThat(JpaSort.of(User_.firstname).and(JpaSort.of(User_.lastname))).contains(Order.asc("firstname"),
+				Order.asc("lastname"));
+	}
+
+	@Test // DATAJPA-12
+	public void combiningSortByMultiplePropertiesWithDifferentSort() {
+
+		assertThat(JpaSort.of(User_.firstname).and(JpaSort.of(DESC, User_.lastname))).contains(Order.asc("firstname"),
+				Order.desc("lastname"));
+	}
+
+	@Test // DATAJPA-12
+	public void combiningSortByNestedEmbeddedProperty() {
+		assertThat(JpaSort.of(path(User_.address).dot(Address_.streetName))).contains(Order.asc("address.streetName"));
+	}
+
+	@Test // DATAJPA-12
+	public void buildJpaSortFromJpaMetaModelSingleAttribute() {
+
+		assertThat(JpaSort.of(ASC, path(User_.firstname))).contains(Order.asc("firstname"));
+	}
+
+	@Test // DATAJPA-12
+	public void buildJpaSortFromJpaMetaModelNestedAttribute() {
+
+		assertThat(JpaSort.of(ASC, path(MailMessage_.mailSender).dot(MailSender_.name)))
+				.contains(Order.asc("mailSender.name"));
+	}
+
+	@Test // DATAJPA-702
+	public void combiningSortByMultiplePropertiesWithDifferentSortUsingSimpleAnd() {
+
+		assertThat(JpaSort.of(User_.firstname).and(DESC, User_.lastname)).containsExactly(Order.asc("firstname"),
+				Order.desc("lastname"));
+	}
+
+	@Test // DATAJPA-702
+	public void combiningSortByMultiplePathsWithDifferentSortUsingSimpleAnd() {
+
+		assertThat(JpaSort.of(User_.firstname).and(DESC, path(MailMessage_.mailSender).dot(MailSender_.name)))
+				.containsExactly(Order.asc("firstname"), Order.desc("mailSender.name"));
+	}
+
+	@Test(expected = IllegalArgumentException.class) // DATAJPA-702
+	public void rejectsNullAttributesForCombiningCriterias() {
+		JpaSort.of(User_.firstname).and(DESC, (Attribute<?, ?>[]) null);
+	}
+
+	@Test(expected = IllegalArgumentException.class) // DATAJPA-702
+	public void rejectsNullPathsForCombiningCriterias() {
+		JpaSort.of(User_.firstname).and(DESC, (Path<?, ?>[]) null);
+	}
+
+	@Test // DATAJPA-702
+	public void buildsUpPathForPluralAttributesCorrectly() {
+
+		// assertThat(JpaSort.of(JpaSort.path(User_.colleagues).dot(User_.roles).dot(Role_.name)), //
+		// hasItem(new Order(ASC, "colleagues.roles.name")));
+	}
+
+	@Test // DATAJPA-965
+	public void createsUnsafeSortCorrectly() {
+
+		JpaSort sort = JpaSort.unsafe(DESC, "foo.bar");
+
+		assertThat(sort).contains(Order.desc("foo.bar"));
+		assertThat(sort.getOrderFor("foo.bar")).isInstanceOf(JpaOrder.class);
+	}
+
+	@Test // DATAJPA-965
+	public void createsUnsafeSortWithMultiplePropertiesCorrectly() {
+
+		JpaSort sort = JpaSort.unsafe(DESC, "foo.bar", "spring.data");
+
+		assertThat(sort).contains(Order.desc("foo.bar"), Order.desc("spring.data"));
+		assertThat(sort.getOrderFor("foo.bar")).isInstanceOf(JpaOrder.class);
+		assertThat(sort.getOrderFor("spring.data")).isInstanceOf(JpaOrder.class);
+	}
+
+	@Test // DATAJPA-965
+	public void combinesSafeAndUnsafeSortCorrectly() {
+
+		// JpaSort sort = JpaSort.of(path(User_.colleagues).dot(User_.roles).dot(Role_.name)).andUnsafe(DESC, "foo.bar");
+		//
+		// assertThat(sort, hasItems(new Order(ASC, "colleagues.roles.name"), new Order(DESC, "foo.bar")));
+		// assertThat(sort.getOrderFor("colleagues.roles.name"), is(not(instanceOf(JpaOrder.class))));
+		// assertThat(sort.getOrderFor("foo.bar"), is(instanceOf(JpaOrder.class)));
+	}
+
+	@Test // DATAJPA-965
+	public void combinesUnsafeAndSafeSortCorrectly() {
+
+		// Sort sort = JpaSort.unsafe(DESC, "foo.bar").and(ASC, path(User_.colleagues).dot(User_.roles).dot(Role_.name));
+		//
+		// assertThat(sort, hasItems(new Order(ASC, "colleagues.roles.name"), new Order(DESC, "foo.bar")));
+		// assertThat(sort.getOrderFor("colleagues.roles.name"), is(not(instanceOf(JpaOrder.class))));
+		// assertThat(sort.getOrderFor("foo.bar"), is(instanceOf(JpaOrder.class)));
+	}
+}
---
+++ src/test/java/org/springframework/data/jpa/repository/UserRepositoryFinderTests.java
@@ -0,0 +1,284 @@
+/*
+ * Copyright 2008-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.repository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.data.domain.Sort.Direction.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.sample.Role;
+import org.springframework.data.jpa.domain.sample.User;
+import org.springframework.data.jpa.repository.sample.RoleRepository;
+import org.springframework.data.jpa.repository.sample.UserRepository;
+import org.springframework.data.repository.query.QueryLookupStrategy;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Integration test for executing finders, thus testing various query lookup strategies.
+ *
+ * @author Oliver Gierke
+ * @see QueryLookupStrategy
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:config/namespace-application-context.xml")
+@Transactional
+public class UserRepositoryFinderTests {
+
+	@Autowired UserRepository userRepository;
+	@Autowired RoleRepository roleRepository;
+
+	User dave, carter, oliver;
+	Role drummer, guitarist, singer;
+
+	@Before
+	public void setUp() {
+
+		drummer = roleRepository.save(new Role("DRUMMER"));
+		guitarist = roleRepository.save(new Role("GUITARIST"));
+		singer = roleRepository.save(new Role("SINGER"));
+
+		dave = userRepository.save(new User("Dave", "Matthews", "dave@dmband.com", singer));
+		carter = userRepository.save(new User("Carter", "Beauford", "carter@dmband.com", singer, drummer));
+		oliver = userRepository.save(new User("Oliver August", "Matthews", "oliver@dmband.com"));
+	}
+
+	@After
+	public void clearUp() {
+
+		userRepository.deleteAll();
+		roleRepository.deleteAll();
+	}
+
+	/**
+	 * Tests creation of a simple query.
+	 */
+	@Test
+	public void testSimpleCustomCreatedFinder() {
+
+		User user = userRepository.findByEmailAddressAndLastname("dave@dmband.com", "Matthews");
+		assertThat(user).isEqualTo(dave);
+	}
+
+	/**
+	 * Tests that the repository returns {@code null} for not found objects for finder methods that return a single domain
+	 * object.
+	 */
+	@Test
+	public void returnsNullIfNothingFound() {
+
+		User user = userRepository.findByEmailAddress("foobar");
+		assertThat(user).isNull();
+	}
+
+	/**
+	 * Tests creation of a simple query consisting of {@code AND} and {@code OR} parts.
+	 */
+	@Test
+	public void testAndOrFinder() {
+
+		List<User> users = userRepository.findByEmailAddressAndLastnameOrFirstname("dave@dmband.com", "Matthews", "Carter");
+
+		assertThat(users).isNotNull();
+		assertThat(users).containsExactlyInAnyOrder(dave, carter);
+	}
+
+	@Test
+	public void executesPagingMethodToPageCorrectly() {
+
+		Page<User> page = userRepository.findByLastname(PageRequest.of(0, 1), "Matthews");
+
+		assertThat(page.getNumberOfElements()).isEqualTo(1);
+		assertThat(page.getTotalElements()).isEqualTo(2L);
+		assertThat(page.getTotalPages()).isEqualTo(2);
+	}
+
+	@Test
+	public void executesPagingMethodToListCorrectly() {
+
+		List<User> list = userRepository.findByFirstname("Carter", PageRequest.of(0, 1));
+		assertThat(list).containsExactly(carter);
+	}
+
+	@Test
+	public void executesInKeywordForPageCorrectly() {
+
+		Page<User> page = userRepository.findByFirstnameIn(PageRequest.of(0, 1), "Dave", "Oliver August");
+
+		assertThat(page.getNumberOfElements()).isEqualTo(1);
+		assertThat(page.getTotalElements()).isEqualTo(2L);
+		assertThat(page.getTotalPages()).isEqualTo(2);
+	}
+
+	@Test
+	public void executesNotInQueryCorrectly() throws Exception {
+
+		List<User> result = userRepository.findByFirstnameNotIn(Arrays.asList("Dave", "Carter"));
+
+		assertThat(result).containsExactly(oliver);
+	}
+
+	@Test // DATAJPA-92
+	public void findsByLastnameIgnoringCase() {
+
+		List<User> result = userRepository.findByLastnameIgnoringCase("BeAUfoRd");
+
+		assertThat(result).containsExactly(carter);
+	}
+
+	@Test // DATAJPA-92
+	public void findsByLastnameIgnoringCaseLike() throws Exception {
+
+		List<User> result = userRepository.findByLastnameIgnoringCaseLike("BeAUfo%");
+
+		assertThat(result).containsExactly(carter);
+	}
+
+	@Test // DATAJPA-92
+	public void findByLastnameAndFirstnameAllIgnoringCase() {
+
+		List<User> result = userRepository.findByLastnameAndFirstnameAllIgnoringCase("MaTTheWs", "DaVe");
+
+		assertThat(result).containsExactly(dave);
+	}
+
+	@Test // DATAJPA-94
+	public void respectsPageableOrderOnQueryGenerateFromMethodName() {
+
+		Page<User> ascending = userRepository.findByLastnameIgnoringCase( //
+				PageRequest.of(0, 10, Sort.by(ASC, "firstname")), //
+				"Matthews" //
+		);
+		Page<User> descending = userRepository.findByLastnameIgnoringCase( //
+				PageRequest.of(0, 10, Sort.by(DESC, "firstname")), //
+				"Matthews" //
+		);
+
+		assertThat(ascending).containsExactly(dave, oliver);
+		assertThat(descending).containsExactly(oliver, dave);
+	}
+
+	@Test // DATAJPA-486
+	public void executesQueryToSlice() {
+
+		Slice<User> slice = userRepository.findSliceByLastname("Matthews", PageRequest.of(0, 1, ASC, "firstname"));
+
+		assertThat(slice.getContent()).containsExactly(dave);
+		assertThat(slice.hasNext()).isTrue();
+	}
+
+	@Test // DATAJPA-1554
+	public void executesQueryToSliceWithUnpaged() {
+
+		Slice<User> slice = userRepository.findSliceByLastname("Matthews", Pageable.unpaged());
+
+		assertThat(slice).containsExactlyInAnyOrder(dave, oliver);
+		assertThat(slice.getNumberOfElements()).isEqualTo(2);
+		assertThat(slice.hasNext()).isEqualTo(false);
+	}
+
+	@Test // DATAJPA-830
+	public void executesMethodWithNotContainingOnStringCorrectly() {
+
+		assertThat(userRepository.findByLastnameNotContaining("u")) //
+				.containsExactly(dave, oliver);
+	}
+
+	@Test // DATAJPA-1519
+	public void parametersForContainsGetProperlyEscaped() {
+
+		assertThat(userRepository.findByFirstnameContaining("liv%")) //
+				.isEmpty();
+	}
+
+	@Test // DATAJPA-1519
+	public void escapingInLikeSpels() {
+
+		User extra = new User("extra", "Matt_ew", "extra");
+
+		userRepository.save(extra);
+
+		assertThat(userRepository.findContainingEscaped("att_")).containsExactly(extra);
+	}
+
+	@Test // DATAJPA-1522
+	public void escapingInLikeSpelsInThePresenceOfEscapeCharacters() {
+
+		User withEscapeCharacter = userRepository.save(new User("extra", "Matt\\xew", "extra1"));
+		userRepository.save(new User("extra", "Matt\\_ew", "extra2"));
+
+		assertThat(userRepository.findContainingEscaped("att\\x")).containsExactly(withEscapeCharacter);
+	}
+
+	@Test // DATAJPA-1522
+	public void escapingInLikeSpelsInThePresenceOfEscapedWildcards() {
+
+		userRepository.save(new User("extra", "Matt\\xew", "extra1"));
+		User withEscapedWildcard = userRepository.save(new User("extra", "Matt\\_ew", "extra2"));
+
+		assertThat(userRepository.findContainingEscaped("att\\_")).containsExactly(withEscapedWildcard);
+	}
+
+	@Test // DATAJPA-829
+	public void translatesContainsToMemberOf() {
+
+		assertThat(userRepository.findByRolesContaining(singer)) //
+				.containsExactlyInAnyOrder(dave, carter);
+
+		assertThat(userRepository.findByRolesContaining(drummer)) //
+				.containsExactly(carter);
+	}
+
+	@Test // DATAJPA-829
+	public void translatesNotContainsToNotMemberOf() {
+
+		assertThat(userRepository.findByRolesNotContaining(drummer)) //
+				.containsExactlyInAnyOrder(dave, oliver);
+	}
+
+	@Test // DATAJPA-974
+	public void executesQueryWithProjectionContainingReferenceToPluralAttribute() {
+
+		assertThat(userRepository.findRolesAndFirstnameBy()) //
+				.isNotNull();
+	}
+
+	@Test(expected = InvalidDataAccessApiUsageException.class) // DATAJPA-1023, DATACMNS-959
+	@Transactional(propagation = Propagation.NOT_SUPPORTED)
+	public void rejectsStreamExecutionIfNoSurroundingTransactionActive() {
+		userRepository.findAllByCustomQueryAndStream();
+	}
+
+	@Test // DATAJPA-1334
+	public void executesNamedQueryWithConstructorExpression() {
+		userRepository.findByNamedQueryWithConstructorExpression();
+	}
+}
